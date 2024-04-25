83a50bef083250042471606ea11c521642bb4605
Prashant Singh <72181295+Prashant830@users.noreply.github.com>
2024-04-25 17:20:14 +0530

    added webview and javascript interface in a for of jdk

--- app/src/main/java/com/example/webviewperformanceimporove/MainActivity.kt
+++ app/src/main/java/com/example/webviewperformanceimporove/MainActivity.kt
@@ -1,20 +1,46 @@
 package com.example.webviewperformanceimporove
 
+import android.annotation.SuppressLint
+import android.content.DialogInterface
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    var WEBVIEW_BASE_URL : String =  "https://main--meek-cuchufli-1de25d.netlify.app/src/html/home.html"
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val webView = findViewById<WebView>(R.id.web)
+        webView.loadUrl(WEBVIEW_BASE_URL)
+
+        // this will enable the javascript.
+        webView.getSettings().javaScriptEnabled = true
+        webView.getSettings().setSupportZoom(true);
+        webView.setWebViewClient(WebViewClient())
+
+        webView.addJavascriptInterface(this, "AndroidInterface");
+
+
+    }
+
+
+    @JavascriptInterface
+    fun showVideoWithJdkMethod(fileUrl : String) {
+        Toast.makeText(this, fileUrl, Toast.LENGTH_SHORT).show()
+    }
+
+
+    @JavascriptInterface
+    fun showVideoWithNdkMethod(fileUrl : String) {
+        Toast.makeText(this, fileUrl, Toast.LENGTH_SHORT).show()
     }
 }
